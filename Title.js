6e7abf4a1b8141097aaa292535841d015bbc0ff3
SahilMakwana18 <159539333+SahilMakwana18@users.noreply.github.com>
2024-02-13 16:54:39 +0530

    Update Title.js
    changed Discription

--- Title.js
+++ Title.js
@@ -4,7 +4,7 @@ class Title extends React.Component{
     render(){
         return(
             <div>
-                This is my classs componentttttt
+                This is my classs component
             </div>
         )
     }
